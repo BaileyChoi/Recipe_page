60e004ae14651ef65aa58e7afb2065f71aaa2689
BaileyChoi <haileycho9981@gmail.com>
2025-03-18 15:56:56 +0900

    Feat: 레시피 페이지 링크 클릭 리스너 생성

---
+++ recipe.js
@@ -0,0 +1,15 @@
+const rs_kimchi = document.getElementById("kimch");
+const rs_miyeokguk = document.getElementById("miyeokguk");
+const rs_bulgalbi = document.getElementById("bulgalbi");
+
+rs_kimchi.addEventListener("click", () => {
+	// 김치 페이지로 이동
+});
+
+rs_miyeokguk.addEventListener("click", () => {
+	// 미역국 페이지로 이동
+});
+
+rs_bulgalbi.addEventListener("click", () => {
+	// 불갈비 페이지로 이동
+});
